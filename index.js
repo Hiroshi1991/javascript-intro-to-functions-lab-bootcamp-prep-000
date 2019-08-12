4a21a870323e3d3d3c5c5f92930bf51534a73c2a
Hiroshi S <sipahioglu_7@hotmail.com>
2019-08-12 15:07:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,4 +21,5 @@ function sayHiToGrandma(string) {
   else if (string === string.toUpperCase()) {
     return "YES INDEED!";
 } else if ('I Love you, Grandma.') {
-    return 'I love you, too.';}
+    return 'I love you, too.';
+}
